7b9c6b31f0b3056a630ef9ce0bbd99afe94ada96
王图思睿 <twothreewang@gmail.com>
2020-10-12 17:26:28 +0800

    增加背景图片遮罩

--- 网易云音乐热评.js
+++ 网易云音乐热评.js
@@ -1,58 +1,143 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: brown; icon-glyph: magic;
-// author:Nicolas-kings
-const res = await getData();
-// 初始化组件ui
-let widget = await createWidget(res)
-// const url = "https://area.sinaapp.com/bingImg/"   //使用必应壁纸作为背景
-// const url = res.music_pic   //使用歌曲封面作为背景
-const url = "http://p1.music.126.net/uarVFKgUlrI9Z1nr-50cAw==/109951162843608471.jpg"     //固定一张图片,这里我选用城南花已开的封面,图片不能太大，容易崩溃
-const i = await new Request(url);
-const img = await i.loadImage();
-widget.backgroundImage = img
-// 如果不是在组件执行，则显示预览
-if (!config.runsInWidget) {
-  await widget.presentMedium()
-}
-// 设置桌面组件
-Script.setWidget(widget)
-Script.complete()
+class Im3xWidget {
+  /**
+   * 初始化
+   */
+  constructor () {
+    this.arg = "网易云音乐@热评"
+    this.widgetSize = config.widgetFamily
+  }
+  /**
+   * 渲染组件
+   */
+  async render () {
+    if (this.widgetSize === 'medium') {
+      return await this.renderSmall()
+    } else if (this.widgetSize === 'large') {
+      return await this.renderLarge()
+    } else {
+      return await this.renderSmall()
+    }
+  }
 
-// 创建组件
-async function createWidget() {
-  let w = new ListWidget()
-  w.backgroundColor = new Color("#222222", 1)
-  //   标题
-  let title = w.addText("\u7f51\u6613\u4e91\u70ed\u8bc4")
-  title.font = Font.boldMonospacedSystemFont(20)
-  title.textColor = Color.white()
-  title.url = "orpheuswidget://"
-  w.addSpacer(8)
+  /**
+   * 渲染小尺寸组件
+   */
+  async renderSmall () {
+    let w = new ListWidget()
+    let data = await this.getData()
+    w = await this.renderHeader(w)
+    w.addSpacer(20)
+    let content = w.addText(data["comments"])
+    content.textColor = Color.white()
+    content.font = Font.lightSystemFont(16)
+    w.backgroundColor = Color.black()
+    w.backgroundImage = await this.shadowImage(await this.getImage(data['music_pic']))
 
-  //   内容
-  let body = w.addText(`❝${res.comments}❞ `)
-  body.font = Font.lightMonospacedSystemFont(18)
-  body.textColor = Color.white()
-  body.textOpacity = 0.88
-  body.url = res.music_url
-  w.addSpacer(8)
+    w.addSpacer(10)
+    let footer = w.addText("—— 《" + data['name'] + '》')
+    footer.rightAlignText()
+    footer.font = Font.lightSystemFont(10)
+    footer.textColor = Color.white()
+    footer.textOpacity = 0.5
 
+    let music_id = data['music_url'].split('id=')[1].split('.')[0]
+    w.url = 'orpheus://song/' + music_id
+    return w
+  }
+  /**
+   * 渲染中尺寸组件
+   */
+  async renderMedium () {
+    let w = new ListWidget()
+    w.addText("Medium")
+    return w
+  }
+  /**
+   * 渲染大尺寸组件
+   */
+  async renderLarge () {
+    let w = new ListWidget()
+    w.addText("暂不支持该尺寸组件")
+    return w
+  }
 
-  let foot = w.addText(`—— 评论来自歌曲「${res.name}」`);
-  foot.font = Font.lightMonospacedSystemFont(12)
-  foot.textColor = Color.orange();
-  foot.textOpacity = 0.88;
-  foot.rightAlignText();
+  async renderHeader (widget) {
+    let header = widget.addStack()
+    let icon = header.addImage(await this.getImage('https://s1.music.126.net/style/favicon.ico'))
+    icon.imageSize = new Size(15, 15)
+    header.addSpacer(10)
+    let title = header.addText("网易云热评")
+    title.textColor = Color.white()
+    title.textOpacity = 0.7
+    title.font = Font.lightSystemFont(14)
+
+    return widget
+  }
+
+  async getData () {
+    let api = 'https://api.66mz8.com/api/music.163.php?format=json'
+    let req = new Request(api)
+    let res = await req.loadJSON()
+    return res
+  }
+
+  /**
+   * 加载远程图片
+   * @param url string 图片地址
+   * @return image
+   */
+  async getImage (url) {
+    let req = new Request(url)
+    return await req.loadImage()
+  }
+
+  /**
+   * 给图片加上半透明遮罩
+   * @param img 要处理的图片对象
+   * @return image
+   */
+  async shadowImage (img) {
+    let ctx = new DrawContext()
+    ctx.size = img.size
+    ctx.drawImageInRect(img, new Rect(0, 0, img.size['width'], img.size['height']))
+    // 图片遮罩颜色、透明度设置
+    ctx.setFillColor(new Color("#000000", 0.7))
+    ctx.fillRect(new Rect(0, 0, img.size['width'], img.size['height']))
+    let res = await ctx.getImage()
+    return res
+  }
   
-  return w
+  /**
+   * 编辑测试使用
+   */
+  async test () {
+    if (config.runsInWidget) return
+    this.widgetSize = 'small'
+    let w1 = await this.render()
+    await w1.presentSmall()
+    this.widgetSize = 'medium'
+    let w2 = await this.render()
+    await w2.presentMedium()
+    this.widgetSize = 'large'
+    let w3 = await this.render()
+    await w3.presentLarge()
+  }
+  
+  /**
+   * 组件单独在桌面运行时调用
+   */
+  async init () {
+    if (!config.runsInWidget) return
+    let widget = await this.render()
+    Script.setWidget(widget)
+    Script.complete()
+  }
 }
 
-async function getData() {
-  const url = 'https://api.66mz8.com/api/music.163.php'
-  const request = new Request(url)
-  const res = await request.loadJSON()
-  console.log(res)
-  return res
+module.exports = Im3xWidget
 
-}
+// 如果是在编辑器内编辑、运行、测试，则取消注释这行，便于调试：
+// await new Im3xWidget().test()
+
+// 如果是组件单独使用（桌面配置选择这个组件使用，则取消注释这一行：
+await new Im3xWidget().init()
